f505a9921f91f77a6ac1602fe37f3bab35522c73
Guilherme Tomazi Klein <guilhermet.klein@procempa.com.br>
2023-10-25 15:15:46 -0300

    teste de mudança

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,7 +2,7 @@ import {type ButtonProps} from "./button.model";
 import * as React from "react";
 
 const Button = (props:ButtonProps) => {
-    return <button>btn-{props.children}</button>
+    return <button>gtk-{props.children}</button>
 }
 
 export default Button
