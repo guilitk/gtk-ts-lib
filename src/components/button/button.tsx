99da1fa0d054867ee5effc8d5c84420aec1b5d81
Guilherme Tomazi Klein <guilhermet.klein@procempa.com.br>
2023-10-25 14:27:03 -0300

    first commit

---
+++ src/components/button/button.tsx
@@ -0,0 +1,8 @@
+import {type ButtonProps} from "./button.model";
+import * as React from "react";
+
+const Button = (props:ButtonProps) => {
+    return <button>{props.children}</button>
+}
+
+export default Button
